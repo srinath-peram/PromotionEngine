6ec0def8a76fae80b2b253f69687d25dda012578
srinath-peram <68001867+srinath-peram@users.noreply.github.com>
2020-07-09 19:38:28 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -74,6 +74,24 @@ namespace PromotionEngine
          public Product(string id)
         {
             this.Id = id;
+             switch (id)
+            {
+                case "A":
+                    this.Price = 50;
+
+                    break;
+                case "B":
+                    this.Price = 30;
+
+                    break;
+                case "C":
+                    this.Price = 20;
+
+                    break;
+                case "D":
+                    this.Price = 15;
+                    break;
+            }
          }
     }
 }
