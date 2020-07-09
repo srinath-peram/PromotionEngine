2a84923fda699208ad72f53e2ec569b362cf7287
srinath-peram <68001867+srinath-peram@users.noreply.github.com>
2020-07-09 19:20:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,13 @@ namespace PromotionEngine
             List<Product> products = new List<Product>();            
             Console.WriteLine("total number of order");
             int a = Convert.ToInt32(Console.ReadLine());
-           
+           for (int i = 0; i < a; i++)
+            {                
+                Console.WriteLine("enter the type of product:A,B,C or D");
+                string type= Console.ReadLine();
+                Product p = new Product(type);
+                products.Add(p);
+            }
         }
        
     }
